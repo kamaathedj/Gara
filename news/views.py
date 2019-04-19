6b33a7f027327a52d0cb988e3a3369232ab6cd44
kamaaTheDj <kamaudavikiruku@gmail.com>
2019-04-19 10:48:39 +0300

    more changes

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from news.models import new
 
 # Create your views here.
+# view 
 def newses():
     k= new.objects.order_by('-createdAt')
     news = []
@@ -21,4 +22,25 @@ def newses():
     
     return  news
    
-       
+
+  def news_detail(request):
+    k= new.objects.order_by('-createdAt')
+    news = []
+    for news1 in k:
+        data = {
+            'id': news1.id,
+            'title': news1.title,
+            'description': news1.description,
+            'image': news1.image,
+            'createdAt':news1.createdAt
+            
+        }
+        news.append(data)
+    context={'news':news}
+   
+    
+   
+    
+    return  render(request,'temps/events-archive.html',context)
+   
+            
